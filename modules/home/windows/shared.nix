025b4453cfd7e7a3cea899dd22d6f79a8f202cdd
Nickel <mailstuffvvv@gmail.com>
2026-06-21 06:30:43 +0300

    feat: add shared.nix to mount shared NTFS partition

---
+++ modules/home/windows/shared.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  fileSystems."/mnt/shared" = {
+    device = "/dev/disk/by-uuid/04623DBB25806C30";
+    fsType = "ntfs3";
+    options = [
+      "rw"
+      "uid=1000"
+      "gid=100"
+      "dmask=0022"
+      "fmask=0133"
+      "windows_names"
+      "exec"
+      "nofail"
+    ];
+  };
+}
